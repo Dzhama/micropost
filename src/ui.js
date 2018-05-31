ac88fd9502e889f3688402fa63012e31ba0f6530
Dzhama <j.shoismatuloev@gmail.com>
2018-05-31 03:20:40 -0400

    Add Post and Show Alert function added

--- src/ui.js
+++ src/ui.js
@@ -16,13 +16,15 @@ class UI {
             <div class="card mb-3">
                 <div class="card-body">
                     <h4 class="card-title">${post.title}</h4>
-                    <p class="card-text">${post.body}</p>
-                    <a href="#" class="edit card-link" data-id=${post.id}>
-                        <i class="fa fa-pencil-alt"></i>
-                    </a>
-                    <a href="#" class="delete card-link" data-id=${post.id}>
-                        <i class="fa fa-trash-alt"></i>
-                    </a>  
+                    <div class="row">
+                        <p class="card-text col-10">${post.body}</p>
+                        <a href="#" class="edit card-link" data-id=${post.id}>
+                            <i class="fa fa-pencil-alt"></i>
+                        </a>
+                        <a href="#" class="delete card-link" data-id=${post.id}>
+                            <i class="fa fa-trash-alt"></i>
+                        </a>
+                    </div>  
                 </div>
             </div>
             `;
@@ -30,7 +32,40 @@ class UI {
 
         this.post.innerHTML = output;
     }
+    
+    showAlert(message, className) {
+        //Create div
+        const div = document.createElement('div');
+        //Add classes
+        div.className = className;
+        //Add text
+        div.appendChild(document.createTextNode(message));
+        //Get parrent
+        const container = document.querySelector('.postContainer');
+        //Get posts
+        const posts = document.querySelector('#posts');
+        //insert alert div
+        container.insertBefore(div, posts)
 
+        //set timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = "";
+        this.bodyInput.value = " ";
+    }
+ 
 }
 
 
